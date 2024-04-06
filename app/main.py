3bba5ea40ffce568e180b57a2777b0aa2c11039d
Edward O'Connor <edwardoc@buffalo.edu>
2024-04-06 18:22:45 -0400

    restructure

---
+++ app/main.py
@@ -0,0 +1,11 @@
+from sensor import MotionSensor
+
+
+print("Hello world.")
+
+
+
+#TODO - Create websocket
+#TODO - Make Basic Frontend
+#TODO - Communicate to frontend with websocket
+
---
+++ app/sensor/motion_sensor.py
@@ -0,0 +1,31 @@
+import RPi.GPIO as GPIO
+import time
+
+class MotionSensor(self):
+    def __init__(self, motionPin):
+        self.motionPin = motionPin
+
+    def runDetector(self):
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(self.motionPin, GPIO.IN)
+
+        print("Starting Motion Detector")
+
+        try:
+            while True:
+                motion = GPIO.input(motionPin)
+                
+                if motion == 1:
+                    print("Motion Detected")
+                else:
+                    print("Looking for motion")
+                
+                time.sleep(.1)
+            
+            
+        except KeyboardInterrupt:
+            GPIO.cleanup()
+            print("GPIO Cleanup done.")
+
+
+    
